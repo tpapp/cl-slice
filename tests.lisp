6f934e573f2e37bcd436bce3c62f1b857a9692d5
tpapp <tkpapp@gmail.com>
2012-12-22 17:52:31 +0100

    Added more tests.

--- tests.lisp
+++ tests.lisp
@@ -16,9 +16,22 @@
     @body))
 
 (deftest test-representation (representation-suite)
+  ;; singletons
   (assert-equalp 1 (slice vec10 1))
   (assert-equalp 9 (slice vec10 -1))
+  (assert-condition error (slice vec10 10))
+  (assert-condition error (slice vec10 nil))
+  (assert-condition error (slice vec10 11))
+  (assert-condition error (slice vec10 -11))
+  ;; ranges
   (assert-equalp #(3 4) (slice vec10 (cons 3 5)))
   (assert-equalp #(7 8 9) (slice vec10 (cons 7 nil)))
   (assert-equalp #(7 8) (slice vec10 (cons 7 -1)))
-  (assert-equalp #(2 3 5) (slice vec10 #(2 3 5))))
+  (assert-condition error (slice vec10 (cons 5 4))) ; not increasing
+  ;; vectors
+  (assert-equalp #(2 3 5) (slice vec10 #(2 3 5)))
+  (assert-equalp #(2 3 3 7) (slice vec10 #(2 3 3 -3)))
+  ;; masks
+  (assert-equalp #(3 4 7) (slice vec10 #*0001100100))
+  (assert-condition error (slice vec10 #*00)) ; too short
+  )

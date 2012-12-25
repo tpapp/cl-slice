a27fd8e56e79710eac2c80d7c19462397d83eed0
tpapp <tkpapp@gmail.com>
2012-12-25 10:39:52 +0100

    Added convenience forms.

--- cl-slice.lisp
+++ cl-slice.lisp
@@ -3,14 +3,18 @@
 
 
 (cl:defpackage #:cl-slice
-  (:use #:cl #:anaphora #:cl-slice-dev)
+  (:use #:cl #:anaphora #:cl-slice-dev #:let-plus)
   (:export
    #:slice
-   #:ref))
+   #:ref
+   #:including
+   #:nodrop
+   #:head
+   #:tail))
 
 (in-package #:cl-slice)
 
-;;; user interface
+;;; generic user interface
 
 (defgeneric ref (object &rest subscripts)
   (:documentation "Return the element of OBJECT specified by SUBSCRIPTS."))
@@ -22,6 +26,40 @@
 
 (defgeneric (setf slice) (value object &rest slices))
 
+;;; some convenience forms
+
+(defstruct including
+  "Range, including both ends."
+  start end)
+
+(defun including (start end)
+  "Range, including both ends."
+  (make-including :start start :end end))
+
+(defmethod canonical-representation (axis (slice including))
+  (let+ (((&structure-r/o including- start end) slice)
+         (start (canonical-representation axis start))
+         (end (canonical-representation axis end)))
+    (canonical-range start (1- end))))
+
+(defstruct nodrop
+  "Select a single index, but don't drop a dimension."
+  index)
+
+(defun nodrop (index)
+  "Slice a single index, but don't drop a dimension."
+  (make-nodrop :index index))
+
+(defmethod canonical-representation (axis (slice nodrop))
+  (let ((start (canonical-representation axis (nodrop-index slice))))
+    (canonical-range start (1+ start))))
+
+(defun head (index)
+  "Slice up to INDEX."
+  (cons 0 index))
+
+(defun tail (index)
+  (cons index nil))
 
 
 

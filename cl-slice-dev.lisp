ce3fe73d91e73000db71ad65320b9a2d00de4142
tpapp <tkpapp@gmail.com>
2013-04-05 09:20:52 +0200

    Removed line breaks.

--- cl-slice-dev.lisp
+++ cl-slice-dev.lisp
@@ -23,20 +23,17 @@
 ;;; resolving slices into canonical representations
 
 (defun canonical-singleton (index)
-  "Canonical representation of a singleton index (a nonnegative integer, which
-is a valid array index)."
+  "Canonical representation of a singleton index (a nonnegative integer, which is a valid array index)."
   (assert (typep index 'array-index))
   index)
 
 (defstruct canonical-range
-  "Canonical representation of a contiguous set of array indices from
-START (inclusive) to END (exclusive)."
+  "Canonical representation of a contiguous set of array indices from START (inclusive) to END (exclusive)."
   (start nil :type array-index)
   (end nil :type array-index))
 
 (defun canonical-range (start end)
-  "Canonical representation of a contiguous set of array indices from
-START (inclusive) to END (exclusive)."
+  "Canonical representation of a contiguous set of array indices from START (inclusive) to END (exclusive)."
   (assert (and (typep start 'array-index)
                (typep end 'array-index)
                (< start end)))
@@ -47,9 +44,7 @@ START (inclusive) to END (exclusive)."
   (vector nil :type (simple-array array-index (*))))
 
 (defun canonical-sequence (sequence)
-  "Canonical representation of array indexes from sequence.  May share
-structure.  Vectors of the upgraded type of (SIMPLE-ARRAY ARRAY-INDEX (*)) are
-preferred for efficiency, otherwise they are coerced."
+  "Canonical representation of array indexes from sequence.  May share structure.  Vectors of the upgraded type of (SIMPLE-ARRAY ARRAY-INDEX (*)) are preferred for efficiency, otherwise they are coerced."
   (let ((vector (coerce sequence '(simple-array array-index (*)))))
     (assert (and (plusp (length vector))
                  (every (lambda (index)
@@ -58,8 +53,7 @@ preferred for efficiency, otherwise they are coerced."
     (make-canonical-sequence :vector vector)))
 
 (defgeneric axis-dimension (axis)
-  (:documentation "Return the dimension of axis.  Needs to be defined for
-non-integer axes."))
+  (:documentation "Return the dimension of axis.  Needs to be defined for non-integer axes."))
 
 (defun slice-reserved-symbol? (symbol)
   "Test if SYMBOL has special semantics for SLICE."
@@ -115,8 +109,7 @@ Unless a specialized method is found, the dimension of the axis is queried with 
                               when (plusp bit) collect index))))
 
 (defun canonical-representations (axes slices)
-  "Return the canonical representations of SLICES given the corresponding
-AXES, checking for matching length."
+  "Return the canonical representations of SLICES given the corresponding AXES, checking for matching length."
   (assert (length= axes slices))
   (mapcar #'canonical-representation axes slices))
 
@@ -131,16 +124,14 @@ AXES, checking for matching length."
   (every #'singleton-representation? representations))
 
 (defun representation-dimension (representation)
-  "Return the dimension of a canonical-representation, or NIL for singleton
-slices (they are dropped)."
+  "Return the dimension of a canonical-representation, or NIL for singleton slices (they are dropped)."
   (aetypecase representation
     (array-index nil)
     (canonical-range (- (canonical-range-end it) (canonical-range-start it)))
     (canonical-sequence (length (canonical-sequence-vector it)))))
 
 (defun representation-dimensions (representations)
-  "Return a list for the dimensions of canonical representations, dropping
-singletons."
+  "Return a list for the dimensions of canonical representations, dropping singletons."
   (loop for r in representations
         for d = (representation-dimension r)
         when d collect d))
@@ -153,8 +144,7 @@ singletons."
     (canonical-sequence (aref (canonical-sequence-vector it) 0))))
 
 (defun representation-iterator (representation carry cons)
-  "Return a closure that sets the car of CONS to the next value each time it
-is called, resetting and calling CARRY when it reaches the end of its range."
+  "Return a closure that sets the car of CONS to the next value each time it is called, resetting and calling CARRY when it reaches the end of its range."
   (flet ((save (value)
            (setf (car cons) value))
          (carry ()
@@ -180,9 +170,7 @@ is called, resetting and calling CARRY when it reaches the end of its range."
                               (save (aref vector position))))))))
 
 (defun row-major-setup (representations terminator)
-  "Return SUBSCRIPTS (a list) and ITERATOR (a closure, no arguments) that
-increments the contents of SUBSCRIPTS.  TERMINATOR is called when all
-subscripts have been visited."
+  "Return SUBSCRIPTS (a list) and ITERATOR (a closure, no arguments) that increments the contents of SUBSCRIPTS.  TERMINATOR is called when all subscripts have been visited."
   (let ((iterator terminator)
         (subscripts (mapcar #'representation-initial-value representations)))
     (loop for r in representations
@@ -201,11 +189,7 @@ subscripts have been visited."
                                      &key index
                                           (setup 'row-major-setup))
                                     &body body)
-  "A macro for traversing representations.  Loops over all possible subscripts
-in REPRESENTAITONS, making them available in SUBSCRIPS during the execution of
-BODY.  The iterator is constructed using the function SETUP (see for example
-ROW-MAJOR-SETUP).  When INDEX is given, a variable with that name is provided,
-containing an index that counts iterations."
+  "A macro for traversing representations.  Loops over all possible subscripts in REPRESENTAITONS, making them available in SUBSCRIPS during the execution of BODY.  The iterator is constructed using the function SETUP (see for example ROW-MAJOR-SETUP).  When INDEX is given, a variable with that name is provided, containing an index that counts iterations."
   (with-unique-names (block-name next)
     `(block ,block-name
        (let+ (((&values ,subscripts ,next)
